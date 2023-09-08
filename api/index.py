3f1a5e9d885eb89c5e2401b26f0a2af69f64846f
Chonwai <chonwaiun@gmail.com>
2023-09-08 16:24:20 +0800

    Update: Updated the event qa normal and steaming API.

--- api/index.py
+++ api/index.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from dotenv import load_dotenv
 from api.services.qabot.qabot import QABotService
 
 load_dotenv()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1",
+    "*",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/python")
@@ -32,7 +52,7 @@ def eventsQAStreaming(query: Optional[str] = None):
                 # Assuming each line is a string and you can directly yield it
                 yield line
 
-        return StreamingResponse(generate(), media_type="text/event-stream")
+        return StreamingResponse(generate(), media_type="text/event-stream", headers={"Cache-Control": "no-cache", "Connection": "keep-alive", "Content-Type": "text/event-stream"})
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
